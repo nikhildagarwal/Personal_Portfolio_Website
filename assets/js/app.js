6ef03f00e7f90979b3948ee2d5f9683cd8a1b16e
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2024-01-07 00:44:28 -0500

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,84 @@
 document.getElementById("theme-btn-id").addEventListener('click',()=>{
     document.body.classList.toggle("light-mode");
 })
+
+var active_btn_ref = document.getElementById("home-btn");
+var active_page_ref = document.getElementById("home");
+var active_page_ref_name = "container header active";
+
+
+document.getElementById("home-btn").addEventListener('click',()=>{
+    let ref = document.getElementById("home-btn");
+    let page_ref = document.getElementById("home");
+    if (ref != active_btn_ref){
+        ref.className = "control active-btn";
+        page_ref.className = "container header active";
+        active_btn_ref.className = "control";
+        active_btn_ref = ref;
+        let parts = active_page_ref_name.split(" ");
+        if(parts.length == 2){
+            active_page_ref.className = parts[0];
+        }else{
+            active_page_ref.className = parts[0] + " "+parts[1];
+        }
+        active_page_ref = page_ref;
+        active_page_ref_name = "container header active";
+    }
+})
+
+document.getElementById("about-btn").addEventListener('click',()=>{
+    let ref = document.getElementById("about-btn");
+    let page_ref = document.getElementById("about");
+    if (ref != active_btn_ref){
+        ref.className = "control active-btn";
+        page_ref.className = "container about active";
+        active_btn_ref.className = "control";
+        active_btn_ref = ref;
+        let parts = active_page_ref_name.split(" ");
+        if(parts.length == 2){
+            active_page_ref.className = parts[0];
+        }else{
+            active_page_ref.className = parts[0] + " "+parts[1];
+        }
+        active_page_ref = page_ref;
+        active_page_ref_name = "container about active";
+    }
+})
+
+document.getElementById("portfolio-btn").addEventListener('click',()=>{
+    let ref = document.getElementById("portfolio-btn");
+    let page_ref = document.getElementById("portfolio");
+    if (ref != active_btn_ref){
+        ref.className = "control active-btn";
+        page_ref.className = "container active";
+        active_btn_ref.className = "control";
+        active_btn_ref = ref;
+        let parts = active_page_ref_name.split(" ");
+        if(parts.length == 2){
+            active_page_ref.className = parts[0];
+        }else{
+            active_page_ref.className = parts[0] + " "+parts[1];
+        }
+        active_page_ref = page_ref;
+        active_page_ref_name = "container active";
+    }
+})
+
+document.getElementById("contact-btn").addEventListener('click',()=>{
+    let ref = document.getElementById("contact-btn");
+    let page_ref = document.getElementById("contact");
+    if (ref != active_btn_ref){
+        ref.className = "control active-btn";
+        page_ref.className = "container contact active";
+        active_btn_ref.className = "control";
+        active_btn_ref = ref;
+        let parts = active_page_ref_name.split(" ");
+        if(parts.length == 2){
+            active_page_ref.className = parts[0];
+        }else{
+            active_page_ref.className = parts[0] + " "+parts[1];
+        }
+        active_page_ref = page_ref;
+        active_page_ref_name = "container contact active";
+    }
+})
