b6a63d215441785907dc337e884cd89f025603a9
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2024-01-07 22:01:26 -0500

    split education and about tabs

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,25 @@ document.getElementById("about-btn").addEventListener('click',()=>{
     }
 })
 
+document.getElementById("edu-btn").addEventListener('click',()=>{
+    let ref = document.getElementById("edu-btn");
+    let page_ref = document.getElementById("edu");
+    if (ref != active_btn_ref){
+        ref.className = "control active-btn";
+        page_ref.className = "container edu active";
+        active_btn_ref.className = "control";
+        active_btn_ref = ref;
+        let parts = active_page_ref_name.split(" ");
+        if(parts.length == 2){
+            active_page_ref.className = parts[0];
+        }else{
+            active_page_ref.className = parts[0] + " "+parts[1];
+        }
+        active_page_ref = page_ref;
+        active_page_ref_name = "container edu active";
+    }
+})
+
 document.getElementById("portfolio-btn").addEventListener('click',()=>{
     let ref = document.getElementById("portfolio-btn");
     let page_ref = document.getElementById("portfolio");
